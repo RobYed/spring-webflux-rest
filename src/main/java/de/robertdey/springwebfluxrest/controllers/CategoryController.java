62ba0a74e4aea24df10141b6557722044714fa3e
Robert Dey <dey@kowalzik.biz>
2018-08-03 10:13:16 +0200

    add HTTP POST method for categories

--- src/main/java/de/robertdey/springwebfluxrest/controllers/CategoryController.java
+++ src/main/java/de/robertdey/springwebfluxrest/controllers/CategoryController.java
@@ -3,17 +3,18 @@ package de.robertdey.springwebfluxrest.controllers;
 import de.robertdey.springwebfluxrest.domain.Category;
 import de.robertdey.springwebfluxrest.repositories.CategoryRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/categories")
+@RequestMapping(CategoryController.API_PATH)
 public class CategoryController {
+
+    static final String API_PATH = "/api/v1/categories";
 
     private final CategoryRepository categoryRepository;
 
@@ -26,4 +27,12 @@ public class CategoryController {
     Mono<Category> getById(@PathVariable String id) {
         return categoryRepository.findById(id);
     }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/")
+    Mono<Void> create(@RequestBody Publisher<Category> categoryPublisher) {
+        return categoryRepository.saveAll(categoryPublisher).then();
+    }
+
+
 }
--- src/test/java/de/robertdey/springwebfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/de/robertdey/springwebfluxrest/controllers/CategoryControllerTest.java
@@ -6,9 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
 
 public class CategoryControllerTest {
 
@@ -29,7 +32,7 @@ public class CategoryControllerTest {
                 .willReturn(Flux.just(Category.builder().description("Cat1").build(),
                         Category.builder().description("Cat 2").build()));
 
-        webTestClient.get().uri("/api/v1/categories/")
+        webTestClient.get().uri(CategoryController.API_PATH + "/")
                 .exchange()
                 .expectBodyList(Category.class)
                 .hasSize(2);
@@ -40,8 +43,23 @@ public class CategoryControllerTest {
         BDDMockito.given(categoryRepository.findById("someid"))
                 .willReturn(Mono.just(Category.builder().description("Cat").build()));
 
-        webTestClient.get().uri("/api/v1/categories/someid")
+        webTestClient.get().uri(CategoryController.API_PATH + "/someid")
                 .exchange()
                 .expectBody(Category.class);
+    }
+
+    @Test
+    public void create() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> catToSaveMono = Mono.just(Category.builder().description("Some Cat").build());
+
+        webTestClient.post()
+                .uri(CategoryController.API_PATH + "/")
+                .body(catToSaveMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
     }
 }
